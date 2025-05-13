8d899682434e64fda3fbfb22bbcee411303078b7
Dorothée Catry <catrydorothee@gmail.com>
2025-05-13 15:42:44 +0200

    tests implementation 2

--- tests/conftests.py
+++ tests/conftests.py
@@ -8,8 +8,6 @@ from app.models.users import User
 from app.core.security import get_password_hash
 import os
 
-# Set test database URL
-os.environ["DATABASE_URL"] = "sqlite:///:memory:"
 
 @pytest.fixture(name="engine")
 def engine_fixture():
